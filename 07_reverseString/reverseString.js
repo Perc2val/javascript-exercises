4720888bf15029d81c24f486187831e1fda5c423
Perc2val <fsb099@aol.de>
2025-11-29 20:28:39 +0100

    07 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let gesplitted = str.split("");
+    let reversed = gesplitted.reverse();
+    let joined = reversed.join("");
+    return joined
 };
 
 // Do not edit below this line
