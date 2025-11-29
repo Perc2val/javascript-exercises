f2d7ecf31a7e60a0c1fa390c5041d32ef0fd26ab
Perc2val <fsb099@aol.de>
2025-11-29 23:18:43 +0100

    09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let array = []
+    if (numOne < 0 || numTwo < 0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)){
+        return "ERROR"
+    }
+    if (numOne > numTwo){
+        lowNum = numTwo;
+        highNum = numOne;
+    } else {
+        lowNum = numOne;
+        highNum = numTwo;
+    }
+    for(let i = lowNum; i <= highNum; i++){
+        array.push(i)
+    }
+    return result = array.reduce((sum, current) => sum + current, 0)
 };
 
 // Do not edit below this line
