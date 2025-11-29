cebbcdf5d8b6e0f693bbb589a1a1937fa3c1790b
Perc2val <fsb099@aol.de>
2025-11-29 23:51:24 +0100

    11 done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return realCelsius = +Number.parseFloat((fahrenheit - 32) * (5/9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return realFahrenheit = +Number.parseFloat((celsius * (9/5)+ 32)).toFixed(1);
 };
 
 // Do not edit below this line
