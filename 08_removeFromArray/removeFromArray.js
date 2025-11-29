9ecfed6bb61708b48a75cff28bc32874a5df937d
Perc2val <fsb099@aol.de>
2025-11-29 22:46:36 +0100

    08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return solution = array.filter(x => !args.includes(x));
 };
 
 // Do not edit below this line
