86289d59d34f4268f4bd84852e7f0a3d8919f7df
Perc2val <fsb099@aol.de>
2025-12-08 18:03:35 +0100

    12 done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current)=> sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((multiply, current)=> multiply * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(i) {
+  let result = 1
+	for(let x = 1; x <= i; x++) {
+    result *= x
+  }
+  return result
 };
 
 // Do not edit below this line
