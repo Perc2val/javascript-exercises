8e9880e31054f539c653ee18fd196e89899c858a
Perc2val <fsb099@aol.de>
2025-12-10 17:41:42 +0100

    16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let timeLived = people.sort((a, b) => ((b.yearOfDeath || 2025) - b.yearOfBirth)-((a.yearOfDeath || 2025) - a.yearOfBirth))
+    return timeLived[0]
+    
 };
 
 // Do not edit below this line
