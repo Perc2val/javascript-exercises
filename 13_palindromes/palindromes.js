499a84bdfa9f638c6b9c429a6651dff4be7ee535
Perc2val <fsb099@aol.de>
2025-12-10 15:37:27 +0100

    13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowString = string.toLowerCase();
+    let punctuation = /[\.,?!" "]/g;
+    let newText = lowString.replace(punctuation, "");
+    let arr = newText.split("");
+    let revArr = arr.reverse()
+    let joinArr = revArr.join("")
+    if (newText === joinArr){
+        return true
+    }
+    return false;
 };
 
 // Do not edit below this line
