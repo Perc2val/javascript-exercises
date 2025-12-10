12a76704678f9d4e14fd978785f789a4de537886
Perc2val <fsb099@aol.de>
2025-12-10 16:13:51 +0100

    14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(stNum) {
+    const fib = [1, 1];
+    let num = +stNum;
+    if (num < 0){
+    return "OOPS"    
+    } else if (num === 0) {
+        return 0
+    }
+    for (let i = 2; i < num; i++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    let lastElement = fib[fib.length -1];
+    return lastElement
 };
 
 // Do not edit below this line
