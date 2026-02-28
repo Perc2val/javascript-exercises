ac2a21a6c4b3a22ec1c03bb558c627d41caa384d
Perc2val <fsb099@aol.de>
2026-02-28 18:36:48 +0100

    09 exercise done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let lowEnd;
+    let highEnd;
+    let arr = [];
+    if (numOne % 1 != 0 || numTwo % 1 != 0 || typeof numOne !== "number" || typeof numTwo !== "number" || numOne < 0 || numTwo < 0){
+        return "ERROR";
+    } else {
+        if (numOne < numTwo){
+            lowEnd = numOne;
+            highEnd = numTwo;
+        } else {
+            highEnd = numOne;
+            lowEnd = numTwo;
+        }
+        for (let i = lowEnd; i <= highEnd; i++){
+            arr.push(i);
+            
+        }
+    }
+    return arr.reduce((accumulator, currentValue)=> accumulator + currentValue, 0)
 };
 
 // Do not edit below this line
